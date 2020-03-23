4fda482947e46b2540ba2564d84ab85869c7bb28
Elison Tuscano <elisontuscano@Elisons-MacBook-Pro.local>
2020-03-22 19:06:01 -0500

    Setup web Interface

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask ,render_template ,request
-
+from io import BytesIO
+import base64, json
+import numpy as np
+from PIL import Image
+from datetime import datetime
 
 app= Flask(__name__)
 
@@ -7,6 +11,21 @@ app= Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/DigitRecognition',methods=['GET','POST'])
+def Execpy():
+    result = {"predict_digit" :"!", "detect_img" :"", "centering_img" :"", "prob" :{}}
+    if request.method == 'POST' :
+        # retrive the image
+        postImg = BytesIO(base64.urlsafe_b64decode(request.form['img']))
+
+        #save digits 
+        '''
+        postImg = Image.open(postImg)
+        postImg.save("./saveImage/{}_save.png".format(datetime.now().strftime('%m-%d_%H.%M.%S')))
+        '''
+    return json.dumps(result)
+    
 
 if __name__== '__main__':
     app.run(debug=True)
