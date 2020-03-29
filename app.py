5962686918c5535dfdeaa94a14bd588feeacfd9a
Elison Tuscano <elisontuscano@Elisons-MacBook-Pro.local>
2020-03-29 00:14:48 -0500

    Added google collab model and tkinter gui

--- app.py
+++ app.py
@@ -4,9 +4,28 @@ import base64, json
 import numpy as np
 from PIL import Image
 from datetime import datetime
+from PIL import ImageGrab, Image
+import numpy as np
+from keras.models import load_model
+
+model = load_model('model/mnist.h5')
 
 app= Flask(__name__)
 
+def predict_digit(img):
+    #img=Image.open(img)
+    #resize image to 28x28 pixels
+    img = img.resize((28,28))
+    #convert rgb to grayscale
+    img = img.convert('L')
+    img = np.array(img)
+    #reshaping to support our model input and normalizing
+    img = img.reshape(1,28,28,1)
+    img = img/255.0
+    #predicting the class
+    print('now predicting the digit')
+    res = model.predict([img])
+    return np.argmax(res), max(res)
 
 @app.route('/')
 def index():
@@ -14,13 +33,22 @@ def index():
 
 @app.route('/DigitRecognition',methods=['GET','POST'])
 def Execpy():
-    result = {"predict_digit" :"!", "detect_img" :"", "centering_img" :"", "prob" :{}}
+    result = {"predict_digit" :"!", "detect_img" :"", "accuracy" :"", "prob" :{}}
     if request.method == 'POST' :
         # retrive the image
         postImg = BytesIO(base64.urlsafe_b64decode(request.form['img']))
-
-        #save digits 
+        
+        
+        #try
+        img=Image.open('saveImage/sample2.png')
+        #predict the digit
+        digit, accuracy = predict_digit(img)
+        result['predict_digit']= digit
+        result['accuracy']=accuracy
+        
         '''
+        #save digits 
+    
         postImg = Image.open(postImg)
         postImg.save("./saveImage/{}_save.png".format(datetime.now().strftime('%m-%d_%H.%M.%S')))
         '''
@@ -28,4 +56,4 @@ def Execpy():
     
 
 if __name__== '__main__':
-    app.run(debug=True)
+    app.run(debug=True,threaded=False)
---
+++ predict.py
@@ -0,0 +1,31 @@
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def predict_digit(img):
+    #img=Image.open(img)
+    #resize image to 28x28 pixels
+    img = img.resize((28,28))
+    #convert rgb to grayscale
+    img = img.convert('L')
+    img = np.array(img)
+    #reshaping to support our model input and normalizing
+    img = img.reshape(1,28,28,1)
+    img = img/255.0
+    img=img.reshape(1,28,28,1)
+    #predicting the class
+    plt.imshow(img.reshape(28,28),cmap=plt.cm.binary)
+    plt.show()
+    print('now predicting the digit')
+    res = model.predict([img])
+    return np.argmax(res), max(res)
+
+
+model = load_model('model/collabmnist.h5')
+
+img=Image.open('saveImage/1.png')
+
+digit, accuracy = predict_digit(img)
+print(digit)
+print(accuracy)
